6011ec583eb94c93880dec8f675cf2b179c645a5
Elias <ewingwold@fmail.com>
2023-10-26 13:50:59 +0200

    Added code
    i fixed my list so the checkbox is infront of the text.
    i made a hover delete button.
    styled alittle bit

--- script.js
+++ script.js
@@ -1,29 +1,60 @@
 let tasks = [];
+const MAX_CHARACTER_LIMIT = 25; // Maximal character limit
 
-function addtext() {
+function addtext(event) {
+    document.getElementById("add_button").addEventListener("click", addtext);
+
+    event.preventDefault();
+    
     let tasktext = document.getElementById("input_felt").value;
+
+    // Max characters reached alert
+    if (tasktext.length > MAX_CHARACTER_LIMIT) {
+        alert("Max characters reached " + MAX_CHARACTER_LIMIT + " characters.");
+        return;
+    }
+
     console.log(tasktext);
 
     tasks.push(tasktext);
 
     document.getElementById("input_felt").value = "";
 
-    let outputElement = document.getElementById("TaskList");
+    renderTasks();
+}
 
+
+function renderTasks() {
+    let outputElement = document.getElementById("TaskList");
     outputElement.innerHTML = "";
 
-    tasks.forEach(function(task) {
+    tasks.forEach(function(task, index) {
         let listItem = document.createElement("li");
-        
-        listItem.style.width = "200px";
-
-        let textNode = document.createTextNode(task);
-        listItem.appendChild(textNode);
+        listItem.className = "task-item";
 
         let checkbox = document.createElement("input");
         checkbox.type = "checkbox";
-        listItem.appendChild(checkbox); // legg checkboxen etter texten
+
+        let textSpan = document.createElement("span");
+        textSpan.className = "task-text";
+        textSpan.appendChild(document.createTextNode(task));
+
+        listItem.appendChild(checkbox);
+        listItem.appendChild(textSpan);
+
+        textSpan.addEventListener("click", function(clickedIndex) {
+            return function() {
+                tasks.splice(clickedIndex, 1);
+                renderTasks();
+            };
+        }(index));
 
         outputElement.appendChild(listItem);
     });
 }
+
+document.getElementById("input_felt").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        addtext(event);
+    }
+});
