2c80dc7aa7d50658034ff80315c4797817a73889
Elias <ewingwold@fmail.com>
2023-10-26 09:58:00 +0200

    code adjustments and added new stuff
    added checkbox
    you can now add more items in the shopping list

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
+let tasks = [];
+
 function addtext() {
     let tasktext = document.getElementById("input_felt").value;
     console.log(tasktext);
-    let testElement = document.getElementById("test");
-    testElement.textContent = tasktext;
+
+    tasks.push(tasktext);
+
+    document.getElementById("input_felt").value = "";
+
+    let outputElement = document.getElementById("TaskList");
+
+    outputElement.innerHTML = "";
+
+    tasks.forEach(function(task) {
+        let listItem = document.createElement("li");
+        
+        listItem.style.width = "200px";
+
+        let textNode = document.createTextNode(task);
+        listItem.appendChild(textNode);
+
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        listItem.appendChild(checkbox); // legg checkboxen etter texten
+
+        outputElement.appendChild(listItem);
+    });
 }
